cbd032ec51926aca062b6e808919939c0d0b6d96
eastlong <1489444965@qq.com>
2024-12-08 18:13:37 +0800

    初始化项目

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
